b9e7f30e74afcce8f337a0a85e1e1b31414bca51
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2023-07-19 18:30:12 +0100

    worked on the carousel

--- starter-code/crew.js
+++ starter-code/crew.js
@@ -1,51 +1,40 @@
 const crewContainer = document.getElementById("crewContainer");
+const carouselItems = document.querySelectorAll(".carousel-item");
 
 function showCrew(crewIndex) {
-    fetch("data.json")
-        .then((response) => response.json())
-        .then((data) => {
-        const crew = data.crew;
-        const crewMember = crew[crewIndex];
-        console.log(crewMember);
-        crewContainer.innerHTML = `
-            <div>
-            <img src="${crewMember.images.png}" class="img-fluid" alt="${crewMember.name}">
-            </div>
-            <div class='crew-cont'>
-            <p class='role'>${crewMember.role}</p>
-            <h2>${crewMember.name}</h2>
-            <p class='desc'>${crewMember.bio}</p>
-            </div>
-        `;
-        })
-        .catch((error) => {
-        console.error("Error:", error);
-        });
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const crew = data.crew;
+      const crewMember = crew[crewIndex];
+      console.log(crewMember);
+      crewContainer.innerHTML = `
+        <div>
+          <img src="${crewMember.images.png}" class="img-fluid" alt="${crewMember.name}">
+        </div>
+        <div class='crew-cont'>
+          <p class='role'>${crewMember.role}</p>
+          <h2>${crewMember.name}</h2>
+          <p class='desc'>${crewMember.bio}</p>
+        </div>
+      `;
+
+      // Remove active class from all carousel items
+      carouselItems.forEach((item) => {
+        item.classList.remove("active");
+      });
+
+      // Add active class to the selected carousel item
+      carouselItems[crewIndex].classList.add("active");
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
-document.getElementById("crewMemberOne").addEventListener("click", () => {
-    showCrew(0);
+// Attach click event listeners to carousel items
+carouselItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    showCrew(index);
+  });
 });
-
-document.getElementById("crewMemberTwo").addEventListener("click", () => {
-    showCrew(1);
-});
-
-document.getElementById("crewMemberThree").addEventListener("click", () => {
-    showCrew(2);
-});
-
-document.getElementById("crewMemberFour").addEventListener("click", () => {
-    showCrew(3);
-});
-
-// fetch("data.json")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     const crew = data.crew;
-//     const crewMember = crew[0];
-//     console.log(crewMember);
-//   })
-//   .catch((error) => {
-//     console.error("Error:", error);
-//   });
