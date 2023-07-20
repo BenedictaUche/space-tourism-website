1dcec5f4577bf5e1cfa28bffc2e948a545b016da
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2023-07-20 23:34:08 +0100

    added mobile responsivity

--- script.js
+++ script.js
@@ -11,10 +11,54 @@ navLinks.forEach(link => {
   });
 });
 
+// function openNav() {
+//   document.querySelector(".sidebar").style.width = "32vw";
+// }
 
+// function closeNav() {
+//   document.querySelector(".sidebar").style.width = "0";
+//   document.querySelector(".main-content").style.marginRight = "0";
+// }
 
 
 const contentContainer = document.getElementById("contentContainer");
+// const destinationHeader = document.getElementById("destinationHeader").addEventListener("click", destinationHeaders);
+
+// function destinationHeaders() {
+//   fetch("data.json")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const destinationHead = data.destinations[0];
+//       const destinationHeadFirst = destinationHead;
+//       console.log(destinationHeadFirst);
+//       contentContainer.innerHTML = `
+//         <div class="">
+//           <img src="${destinationHeadFirst.images.png}" class="img-fluid" alt="${destinationHeadFirst.name}">
+//         </div>
+//         <div class="content-one">
+//           <h2>${destinationHeadFirst.name}</h2>
+//           <p class="desc">${destinationHeadFirst.description}</p>
+//           <div class="line"></div>
+//           <div class="distance-travel">
+//             <div>
+//               <p class="fs-6">AVG. DISTANCE</p>
+//               <p class="fs-4">${destinationHeadFirst.distance}</p>
+//             </div>
+//             <div>
+//               <p class="fs-6">EST. TRAVEL TIME</p>
+//               <p class="fs-4">${destinationHeadFirst.travel}</p>
+//             </div>
+//           </div>
+//         </div>
+//       `;
+//     })
+//     .catch((error) => {
+//       console.error("Error:", error);
+//     });
+// }
+
+
+
 
 
 function showDestination(destinationIndex) {
@@ -25,7 +69,7 @@ function showDestination(destinationIndex) {
       const destination = destinations[destinationIndex];
       console.log(destination);
       contentContainer.innerHTML = `
-        <div class="">
+        <div class="destination-image">
           <img src="${destination.images.png}" class="img-fluid" alt="${destination.name}">
         </div>
         <div class="content-one">
@@ -35,11 +79,11 @@ function showDestination(destinationIndex) {
           <div class="distance-travel">
             <div>
               <p class="fs-6">AVG. DISTANCE</p>
-              <p class="fs-4">${destination.distance}</p>
+              <h2 class="fs-4">${destination.distance}</h2>
             </div>
             <div>
               <p class="fs-6">EST. TRAVEL TIME</p>
-              <p class="fs-4">${destination.travel}</p>
+              <h2 class="fs-4">${destination.travel}</h2>
             </div>
           </div>
         </div>
